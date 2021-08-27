9eaf96010355316510750b8e39f9c8d5c011f3cb
naomipham <69292170+naomipham@users.noreply.github.com>
2021-08-27 19:58:31 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bewd-week2
+ 
---
+++ public/db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE ben_test;
+
+use ben_test;
+
+CREATE TABLE works (
+	id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	artistname VARCHAR(30) NOT NULL,
+	worktitle VARCHAR(50) NOT NULL,
+	workdate VARCHAR(30),
+    worktype VARCHAR(30),
+	date TIMESTAMP
+);
+
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
